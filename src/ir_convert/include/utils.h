69b31b8e66bf24c7231a7521064e1c094ae5bfe7
GCaptainNEmo <1136003685@qq.com>
2021-10-31 21:47:04 +0800

    modify utils.h

--- src/ir_convert/include/utils.h
+++ src/ir_convert/include/utils.h
@@ -1,41 +1,7 @@
-/*
- *
- * Copyright (c) 2013, Simon Lynen, ASL, ETH Zurich, Switzerland
- * You can contact the authors at <firstname.lastname at ethz dot ch>
- *
- * All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
-#ifndef FALSECOLOR_H_
-#define FALSECOLOR_H_
+#ifndef UTILS_H_
+#define UTILS_H_
 
 #include <opencv2/core/core.hpp>
-// #include <opencv2/contrib/retina.hpp>
-// #include <opencv2/bioinspired/retina.hpp>
 #include <boost/shared_ptr.hpp>
 
 class converter_16_8
@@ -92,4 +58,4 @@ palette GetPalette(const std::string & pal);
 
 void convertFalseColor(const cv::Mat& srcmat, cv::Mat& dstmat, const palette &pal, bool drawlegend = false, double mintemp = 0, double maxtemp = 0);
 
-#endif /* FALSECOLOR_H_ */
+#endif /* UTILS_H_ */
